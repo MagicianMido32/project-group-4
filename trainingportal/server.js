539cd3dac50fe922026f73916cf67f08a1d91922
HossamElnems <109301368+HossamElnems@users.noreply.github.com>
2022-07-22 18:01:49 +0200

    Update server.js

--- trainingportal/server.js
+++ trainingportal/server.js
@@ -15,6 +15,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 
  */
+
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
